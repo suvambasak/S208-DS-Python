f2e9fdbae68899f76795ba47196de8a9c25b8458
Suvam Basak <suvambasak2008@gmail.com>
2024-01-12 15:19:19 +0530

    Lecture 2 complete

--- Backgrounds/scopes.py
+++ Backgrounds/scopes.py
@@ -1,15 +1,16 @@
 global_var = 'I am global variable'
 
-def f1() -> None:
-    print (global_var)
 
-    # global global_var
-    # global_var = 'Changed by f1'
-    # print (global_var)
+def f1() -> None:
+    # print(global_var)
+
+    global global_var
+    global_var = 'Changed by f1'
+    print(global_var)
+
 
 def f2() -> None:
     local_var_f2 = 'I am local at f2'
-    
 
 
 def f3() -> None:
@@ -20,11 +21,10 @@ def f3() -> None:
         local_var_f3 += '. Changed!'
 
     change_local_var_f3()
-    print (local_var_f3)
+    print(local_var_f3)
 
 
-
-# print (global_var)
-# print (local_var_f2)
-# f1()
-# f3()
+# print(local_var_f2)
+f1()
+print(global_var)
+f3()
